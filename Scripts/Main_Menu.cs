1a0d4a0a19a31a149e54a206acfe017e7d39fb0a
Mohammad Malik <55553028+TempestTenebris@users.noreply.github.com>
2025-03-18 19:35:16 +0000

    Fixed settings menu and main menu buttons

--- Scripts/Main_Menu.cs
+++ Scripts/Main_Menu.cs
@@ -6,8 +6,8 @@ public partial class Main_Menu : Control
     private Button Start_Button;
     private Button Options_Button;
     private Button Exit_Button;
-    private PackedScene PlayerSelect;
-    private PackedScene OptionSelect;
+    //private PackedScene PlayerSelect;
+    //private PackedScene OptionSelect;
 
     public override void _Ready()
     {
@@ -15,8 +15,8 @@ public partial class Main_Menu : Control
         Options_Button = GetNode<Button>("MarginContainer/HBoxContainer/VBoxContainer/Settings_Button");
         Exit_Button = GetNode<Button>("MarginContainer/HBoxContainer/VBoxContainer/Exit_Button");
 
-        PlayerSelect = (PackedScene)GD.Load("res://Scenes/PlayerSelection.tscn"); //add player select link after
-        OptionSelect = (PackedScene)GD.Load(""); //add options select link after
+        //PlayerSelect = (PackedScene)GD.Load("res://Scenes/PlayerSelection.tscn"); 
+        //OptionSelect = (PackedScene)GD.Load("res://Scenes/Settings_Menu.tcsn"); 
         
         Start_Button.Pressed += OnStartPressed;
         Options_Button.Pressed += OnOptionsPressed;
@@ -25,18 +25,19 @@ public partial class Main_Menu : Control
 
     private void OnStartPressed()
     {
-        //SceneManager.Instance.PreviousScenePath = "es://Scenes/Main_Menu.tscn";
-        //SceneManager.Instance.ChangeScene("es://Scenes/Settings_Menu.tscn");
+        // Record that we're coming from the Main Menu.
+        SceneManager.Instance.PreviousScenePath = "res://Scenes/Main_Menu.tscn";
+        SceneManager.Instance.ChangeScene("res://Scenes/PlayerSelection.tscn");
 
-        GetTree().ChangeSceneToPacked(PlayerSelect);
+        //GetTree().ChangeSceneToPacked(PlayerSelect);
     }
 
     private void OnOptionsPressed()
     {
         // Record that we're coming from the Main Menu.
-        //SceneManager.Instance.PreviousScenePath = "es://Scenes/Main_Menu.tscn";
-        //SceneManager.Instance.ChangeScene("es://Scenes/Settings_Menu.tscn");
-        GetTree().ChangeSceneToPacked(OptionSelect);
+        SceneManager.Instance.PreviousScenePath = "res://Scenes/Main_Menu.tscn";
+        SceneManager.Instance.ChangeScene("res://Scenes/Settings_Menu.tscn");
+        //GetTree().ChangeSceneToPacked(OptionSelect);
     }
 
     private void OnExitPressed()
